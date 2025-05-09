1d033bb185a18b5347241bcdaaea970f8c8dfc03
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-05-09 23:29:08 +0530

    db connection with postgress and typeORM

--- project/src/posts/posts.service.ts
+++ project/src/posts/posts.service.ts
@@ -1,44 +1,54 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
-import { Posts } from './interfaces/post.interface'
+// import { Post } from './interfaces/post.interface'
+import { Post } from './entities/post.entity'
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UpdatePostDto } from './dto/update-post.dto'
+import { CreatePostDto } from './dto/create-post.dto'
 
 @Injectable()
 export class PostsService {
-  private posts: Posts[] = [
-    {
-      id: 1,
-      title: 'Post 1',
-      content: 'Content of post 1',
-      authorName: 'Author 1',
-      createdAt: new Date(),
-    },
-    {
-      id: 2,
-      title: 'Post 2',
-      content: 'Content of post 2',
-      authorName: 'Author 2',
-      createdAt: new Date(),
-    },
-    {
-      id: 3,
-      title: 'Post 3',
-      content: 'Content of post 3',
-      authorName: 'Author 3',
-      createdAt: new Date(),
-    },
-  ]
+  // private posts: Post[] = [
+  //   {
+  //     id: 1,
+  //     title: 'Post 1',
+  //     content: 'Content of post 1',
+  //     authorName: 'Author 1',
+  //     createdAt: new Date(),
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Post 2',
+  //     content: 'Content of post 2',
+  //     authorName: 'Author 2',
+  //     createdAt: new Date(),
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Post 3',
+  //     content: 'Content of post 3',
+  //     authorName: 'Author 3',
+  //     createdAt: new Date(),
+  //   },
+  // ]
 
-  private getNextId (): number {
-    return this.posts.length > 0
-      ? Math.max(...this.posts.map(post => post.id)) + 1
-      : 1
+  constructor (
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
+
+  // private getNextId (): number {
+  //   return this.posts.length > 0
+  //     ? Math.max(...this.posts.map(post => post.id)) + 1
+  //     : 1
+  // }
+
+  async findall (): Promise<Post[]> {
+    return await this.postsRepository.find()
   }
 
-  findall (): Posts[] {
-    return this.posts
-  }
-
-  findById (id: number): Posts {
-    const post = this.posts.find(post => post.id === id)
+  async findById (id: number): Promise<Post> {
+    const post = await this.postsRepository.findOneBy({ id })
 
     if (!post) {
       throw new NotFoundException(`Post with id ${id} not found`)
@@ -46,44 +56,41 @@ export class PostsService {
     return post
   }
 
-  create (createPostData: Omit<Posts, 'id' | 'createdAt'>): Posts {
-    const newPost: Posts = {
-      id: this.getNextId(),
-      ...createPostData,
-      createdAt: new Date(),
-    }
-
-    this.posts.push(newPost)
-    return newPost
+  async create (
+    createPostData: CreatePostDto,
+  ): Promise<Post> {
+    const newlyCreatedPost = this.postsRepository.create({
+      title: createPostData.title,
+      content: createPostData.content,
+      authorName: createPostData.authorName,
+    })
+    return this.postsRepository.save(newlyCreatedPost)
   }
 
-  update (
+  async update (
     id: number,
-    updatePostData: Partial<Omit<Posts, 'id' | 'createdAt'>>,
-  ): Posts {
-    const currentPostIndex = this.posts.findIndex(post => post.id === id)
-    if (currentPostIndex === -1) {
+    updatePostData: UpdatePostDto,
+  ): Promise<Post> {
+    const currentPost = await this.findById(id)
+    if (!currentPost) {
       throw new NotFoundException(`Post with id ${id} not found`)
     }
 
-    this.posts[currentPostIndex] = {
-      ...this.posts[currentPostIndex],
-      ...updatePostData,
-      updatedAt: new Date(),
+    if(updatePostData.title) {
+      currentPost.title = updatePostData.title
     }
-    return this.posts[currentPostIndex]
+    if(updatePostData.content) {
+      currentPost.content = updatePostData.content
+    }
+    if(updatePostData.authorName) {
+      currentPost.authorName = updatePostData.authorName
+    }
+    return this.postsRepository.save(currentPost)
   }
 
-  delete (id: number): { message: string } {
-    const currentPostIndex = this.posts.findIndex(post => post.id === id)
-    if (currentPostIndex === -1) {
-      throw new NotFoundException(`Post with id ${id} not found`)
-    }
-
-    this.posts.splice(currentPostIndex, 1)
-
-    return {
-      message: `Post with id ${id} deleted successfully`,
-    }
+  async delete (id: number): Promise<void> {
+    const currentPost = await this.findById(id)
+    
+    await this.postsRepository.remove(currentPost)
   }
 }
