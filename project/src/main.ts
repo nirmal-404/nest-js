517abc3bdc32bd794ec713e4559dbf7aa1b68ad6
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-05-09 18:27:03 +0530

    pipes dto and validations

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap () {
+  const app = await NestFactory.create(AppModule)
+
+  // validating incomming request bodies automatically
+  // we can use these validations individually in controllers as well
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,  // strips properties that do not have decorators
+      forbidNonWhitelisted: true,
+      transform: true,  // automatically transform payloads to be objects typed according to their DTO classes
+      disableErrorMessages: false,  // disable error messages in production
+    }),
+  )
+
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
