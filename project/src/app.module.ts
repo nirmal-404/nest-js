f6016cad8f21c95caf79e0f335dd186e4f57c116
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-05-11 01:11:05 +0530

    rate limiting and throttling

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PostsModule } from './posts/posts.module'
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from './posts/entities/post.entity';
-import { AuthModule } from './auth/auth.module';
-import { User } from './auth/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Post } from './posts/entities/post.entity'
+import { AuthModule } from './auth/auth.module'
+import { User } from './auth/entities/user.entity'
+import { ThrottlerModule } from '@nestjs/throttler'
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 5,
+      },
+    ]),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -16,7 +23,7 @@ import { User } from './auth/entities/user.entity';
       username: 'postgres',
       password: 'root',
       database: 'nestjs-node-4',
-      entities: [Post, User],// array of entities to be used by TypeORM
+      entities: [Post, User], // array of entities to be used by TypeORM
       synchronize: true, // dev mode only, do not use in production
     }),
     PostsModule,
